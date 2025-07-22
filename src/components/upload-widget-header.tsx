e5eee8a3ceb9b07ebc249ef3f6712922f294e244
Gabriela <gabriela.carniel@sellflux.com.br>
2025-07-22 14:50:04 -0300

    feat: integrate Radix UI collapsible component and update upload widget structure

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,12 +1,16 @@
 import { Minimize2 } from "lucide-react";
+import { Button } from "./ui/buttom";
+import * as Collapsible from "@radix-ui/react-collapsible";
 
 export function UploadWidgetHeader() {
   return (
     <div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between">
       <span className="text-sm font-medium">Upload files</span>
-      <button type="button">
-        <Minimize2 strokeWidth={1.5} className="size-4" />
-      </button>
+      <Collapsible.Trigger asChild>
+        <Button type="button" size="icon" className="-mr-2">
+          <Minimize2 strokeWidth={1.5} className="size-4" />
+        </Button>
+      </Collapsible.Trigger>
     </div>
   );
 }
