7f6bf344e6ecc75e6eac10eeb4c2d7c043f66f59
Gabriela <gabriela.carniel@sellflux.com.br>
2025-07-28 14:58:54 -0300

    feat: add motion animations to upload widget components

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,5 +1,6 @@
 import { useDropzone } from "react-dropzone";
 import { CircularProgressBar } from "./ui/circular-progress-bar";
+import { motion } from "motion/react";
 
 export function UploadWidgetDropzone() {
   const isThereAnyPendingUpload = false;
@@ -18,7 +19,14 @@ export function UploadWidgetDropzone() {
   });
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <motion.div
+      className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{ duration: 0.4 }}
+    >
       <div
         data-active={isDragActive}
         className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
@@ -46,6 +54,6 @@ export function UploadWidgetDropzone() {
       <span className="text-xxs text-zinc-400">
         Only PNG and JPG files are supported.
       </span>
-    </div>
+    </motion.div>
   );
 }
--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,19 +1,41 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
-import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 import { UploadWidgetHeader } from "./upload-widget-header";
-import { useState } from "react";
-import { UploadWidgetMinimizedButton } from "./upload.widget-minimized-button";
+import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
+import { motion, useCycle } from "motion/react";
 
 export function UploadWidget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-  return (
-    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-      <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
-      {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+  const isThereAnyPendindUploads = true
+  const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
-       <Collapsible.Content>
-        <UploadWidgetHeader />
+  return (
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+      <motion.div
+        data-progress={isThereAnyPendindUploads}
+        className="bg-zinc-900 overflow-hidden rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+        animate={isWidgetOpen ? "open" : "closed"}
+        variants={{
+          closed: {
+            width: "max-content", // TODO: check why is not returning to max-content when closed
+            height: 44,
+            transition: {
+              type: "inertia",
+            },
+          },
+          open: {
+            width: 360,
+            height: "auto",
+            transition: {
+              duration: 0.1,
+            },
+          },
+        }}
+      >
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+
+        <Collapsible.Content>
+          <UploadWidgetHeader />
 
           <div className="flex flex-col gap-4 py-3">
             <UploadWidgetDropzone />
@@ -22,8 +44,8 @@ export function UploadWidget() {
 
             <UploadWidgetUploadList />
           </div>
-       </Collapsible.Content>
-      </div>
+        </Collapsible.Content>
+      </motion.div>
     </Collapsible.Root>
   );
 }
